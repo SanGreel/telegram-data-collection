f9d69f13e2ef95a1860a09c0bfe2fe3312ae5de3
Ÿuliia Syrenkova <gofman.jj@gmail.com>
2020-03-14 19:53:45 +0200

    Iterration of telegram dialogs

--- tg_client.py
+++ tg_client.py
@@ -3,5 +3,15 @@ from telethon import TelegramClient, events, sync
 api_id = 1310963
 api_hash = 'c3647146b1aeb55cae072386b07b08dc'
 
-client = TelegramClient('session name', api_id, api_hash)
-client.start()
+client = TelegramClient('julia', api_id, api_hash)
+
+
+async def main():
+    async for dialog in client.iter_dialogs():
+        print('{:>14}: {}'.format(dialog.id, dialog.title))
+
+    # message = await client.get_messages(ids)
+    # print(message)
+
+with client:
+    client.loop.run_until_complete(main())
