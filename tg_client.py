807df26f9c03cb0452dafd69bc0e7e2f75be5e4b
Ÿuliia Syrenkova <gofman.jj@gmail.com>
2020-03-16 12:43:02 +0200

    Add a config file

--- tg_client.py
+++ tg_client.py
@@ -1,28 +1,39 @@
+import configparser
+import json
+
+
 from telethon import TelegramClient, events, sync
 
+# api_id = 1310963
+# api_hash = 'c3647146b1aeb55cae072386b07b08dc'
+session_name = 'tmp'
 
 
 if __name__ == "__main__":
-    api_id = 1310963
-    api_hash = 'c3647146b1aeb55cae072386b07b08dc'
+
+    with open('config.json') as json_file:
+        config = json.load(json_file)
+
+    api_id = config['api_id']
+    api_hash = config['api_hash']   
 
     msg_folder = 'data/msg/'
 
     j_and_a_dialog_id = 325314319
     msg_limit = 10
 
-    session_name = 'tmp2'
-
     client = TelegramClient(session_name, api_id, api_hash)
 
     async def main():
         dialogs = await client.get_dialogs()
-
+        
+        # Getting id for each dialog in the list of dialogs  
         for d in dialogs:
             id = d.id
 
             text = ''
 
+        # Entity = объект, in this case, it's a dialog id and we pass it to the 'get_massages' method
             channel_entity = await client.get_entity(id)
             messages = await client.get_messages(channel_entity, limit=msg_limit)
 
