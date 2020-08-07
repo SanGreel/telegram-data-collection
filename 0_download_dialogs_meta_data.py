13f38e01279f170eaad47553b33ca5bc75218b34
Denys <herasymuk@ucu.edu.ua>
2020-08-07 19:19:43 +0300

    fix all tasks with 0_download_dialogs_meta_data.py

--- 0_download_dialogs_meta_data.py
+++ 0_download_dialogs_meta_data.py
@@ -1,11 +1,94 @@
 import argparse
+import json
 import os
 
-from utils.utils import init_config, init_tg_client, save_dialog
+import telethon
+from telethon import TelegramClient
+
+from utils.utils import init_config, save_dialog
 
 
-if __name__ == "__main__":
+async def save_user_dialogs(client, dialogs_limit):
+    dialogs = await client.get_dialogs()
 
+    # Getting id for each dialog in the list of dialogs
+    for n_dialog, dialog in enumerate(dialogs):
+        if dialogs_limit == n_dialog:
+            exit(0)
+        print(f"step #{n_dialog + 1}")
+
+        dialog_id = dialog.id
+        name_of_dialog = dialog.name
+        users_names = []
+
+        type_of_dialog = ""
+        if dialog.is_user:
+            type_of_dialog = "Private dialog"
+        elif dialog.is_group:
+            type_of_dialog = "Group"
+        elif dialog.is_channel:
+            type_of_dialog = "Channel"
+
+        try:
+            users = await client.get_participants(dialog)
+
+            for user in users:
+                if user.username is not None:
+                    user_dict = {
+                        "first_name": user.first_name,
+                        "last_name": user.last_name,
+                        "username": user.username,
+                        "phone": user.phone,
+                    }
+                    users_names.append(user_dict)
+
+            save_dialog(dialog_id, name_of_dialog, users_names, type_of_dialog)
+
+        except TypeError:
+            print("ERROR!!!!! with getting participants of the dialog")
+            save_dialog(dialog_id, name_of_dialog, users_names, type_of_dialog)
+
+        except telethon.errors.rpcerrorlist.ChatAdminRequiredError as error:
+            print("ERROR!!!!! ", error)
+            save_dialog(dialog_id, name_of_dialog, users_names, type_of_dialog)
+
+
+def make_config_client_access(config_path, session_name):
+    if not os.path.exists(config_path):
+        api_id, api_hash = "", ""
+
+        # create config.json file
+        while not api_id.isdigit() or not api_hash:
+            api_id = input("Input your api_id: ")
+            if "q" == api_id:
+                break
+            api_hash = input("Input your api_hash: ")
+            if "q" == api_hash:
+                break
+
+        with open(
+            os.path.join("config", "config_example.json"), "r", encoding="utf-8"
+        ) as json_file:
+            config = json.load(json_file)
+
+        config["api_id"] = api_id
+        config["api_hash"] = api_hash
+
+    else:
+        config = init_config(config_path)
+
+    client = TelegramClient(session_name, config["api_id"], config["api_hash"])
+
+    if not os.path.exists(config["dialogs_metadata_folder"]):
+        os.mkdir(config["dialogs_metadata_folder"])
+
+    with open(config_path, "w", encoding="utf-8") as json_file:
+        json.dump(config, json_file, indent=4, ensure_ascii=True)
+
+    return client
+
+
+if __name__ == "__save_user_dialogs__":
     parser = argparse.ArgumentParser(
         description="Download dialogs meta data for account."
     )
@@ -17,11 +100,12 @@ if __name__ == "__main__":
         "--config_path",
         type=str,
         help="path to config file",
-        default="config/config.json",
+        default=os.path.join("config", "config.json"),
     )
     parser.add_argument("--debug_mode", type=int, help="Debug mode", default=0)
     parser.add_argument("--session_name", type=str, help="session name", default="tmp")
 
+    # read arguments from terminal
     args = parser.parse_args()
 
     CONFIG_PATH = args.config_path
@@ -29,87 +113,7 @@ if __name__ == "__main__":
     DIALOGS_LIMIT = args.dialogs_limit
     SESSION_NAME = args.session_name
 
-    j_and_a_dialog_id = 331192040
-
-    config = init_config(CONFIG_PATH)
-    client = init_tg_client(SESSION_NAME, config["api_id"], config["api_hash"])
-
-    if not os.path.exists(config["dialogs_metadata_folder"]):
-        os.mkdir(config["dialogs_metadata_folder"])
-
-    # TODO: fix problem with msg, and meta_folder. They aren't variables now, they are in the config var
-
-    async def main():
-        dialogs = await client.get_dialogs()
-
-        k = 1
-
-        # Getting id for each dialog in the list of dialogs
-        for d in dialogs:
-            # print(DIALOGS_LIMIT)
-            if DIALOGS_LIMIT != -1 and k > DIALOGS_LIMIT:
-                exit(0)
-            print(f"step #{k}")
-
-            k += 1
-
-            dialog_id = d.id
-            name_of_dialog = d.name
-            users_names = []
-
-            if d.is_user == True:
-                type_of_dialog = "Private dialog"
-            elif d.is_group == True:
-                type_of_dialog = "Group"
-            elif d.is_channel == True:
-                type_of_dialog = "Channel"
-
-            # TODO: 3. fix downloading of users list, only exception branch works now
-            try:
-                async for u in client.get_participants(d):
-                    print(dialog_id, name_of_dialog)
-                    # save_dialog(dialog_id, name_of_dialog, users_names, type_of_dialog)
-
-            # TODO: 4. add proper exception
-            except:
-                save_dialog(dialog_id, name_of_dialog, users_names, type_of_dialog)
-
-                print(f"ChatAdminRequiredError for {name_of_dialog}")
-                print("\n\n")
-
-    # # both arguments are empty
-    # if SHOW_DIALOGS is None \
-    #         and DIALOG_ID is None:
-    #     # print('ERROR: at least one argument should be passed')
-    #     raise ValueError('ERROR: at least one argument should be passed')
-    #
-    # if not os.path.exists(config['msg_folder']):
-    #     os.mkdir(config['msg_folder'])
-    #
-    # # TODO: check if can be improved
-    # if MSG_LIMIT == -1:
-    #     MSG_LIMIT = 1000000000
-    #
-    #
-    #
-    # # show dialogs list
-    # if SHOW_DIALOGS is not None:
-    #     number_of_dialogs_to_show = SHOW_DIALOGS
-    #
-    #     if number_of_dialogs_to_show > len(dialogs_list):
-    #         number_of_dialogs_to_show = len(dialogs_list)
-    #
-    #     show_dialogs(number_of_dialogs_to_show)
-    #
-    # # download single dialog          -1001314742214
-    #                                          -1001314742214
-
-    # parser.add_argument('--show_dialogs', type=int, help='number of dialogs to show', default=None)
-    # def show_dialogs(n):
-    #     for i in range(0, number_of_dialogs_to_show):
-    #         dialog = dialogs_list[i]
-    #         print(f"ID #{dialog['id']}")
-    #         print(f"{dialog['name']}")
-    #         print('\n')
+    # start saving dialogs of the special user
+    client = make_config_client_access(CONFIG_PATH, SESSION_NAME)
     with client:
-        client.loop.run_until_complete(main())
+        client.loop.run_until_complete(save_user_dialogs(client, DIALOGS_LIMIT))
--- utils/utils.py
+++ utils/utils.py
@@ -2,18 +2,17 @@ import os
 import json
 from glob import glob
 
-from telethon import TelegramClient, events, sync, errors
+# from telethon import TelegramClient, events, sync, errors
 
 
 def init_config(config_path):
     with open(config_path) as json_file:
         config = json.load(json_file)
         return config
-    return None
 
 
-def init_tg_client(session_name, api_id, api_hash):
-    return TelegramClient(session_name, api_id, api_hash)
+# def init_tg_client(session_name, api_id, api_hash):
+#     return TelegramClient(session_name, api_id, api_hash)
 
 
 def read_dialogs(metadata_folder="data/dialogs_meta/", metadata_format="json"):
@@ -33,16 +32,15 @@ def read_dialogs(metadata_folder="data/dialogs_meta/", metadata_format="json"):
 
 
 def save_dialog(dialog_id, name_of_dialog, users_names, type_of_dialog):
-    # TODO: 2. fix encoding problem
     metadata = {
         "id": dialog_id,
         "name": name_of_dialog,
-        "users": users_names,
         "type": type_of_dialog,
+        "users": users_names
     }
 
     dialog_file_path = "data/dialogs_meta/" + str(dialog_id) + ".json"
-    with open(dialog_file_path, "w+", encoding="utf8") as meta_file:
-        json.dump(metadata, meta_file)
+    with open(dialog_file_path, "w", encoding="utf8") as meta_file:
+        json.dump(metadata, meta_file, indent=4, ensure_ascii=False)
         print(f"saved {dialog_file_path}")
         print("\n")
