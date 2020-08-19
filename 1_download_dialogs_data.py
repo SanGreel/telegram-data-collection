27669876a1e67c8a34bbae4d8b0710e5e0174721
Mark Valyavka <v221101mark@gmail.com>
2020-08-19 23:38:11 +0300

    retrieve info about voice, video, stickers, photos; removed unnecessary save_dialog fucntion call from meta_dialog_retrival

--- 1_download_dialogs_data.py
+++ 1_download_dialogs_data.py
@@ -69,6 +69,50 @@ def msg_limit_input_handler(msg_limit):
     return msg_limit
 
 
+def msg_handler(msg):
+    """
+    Handles attributes of a specific message, depending if
+    it is text, photo, voice, video or sticker
+
+    :param msg: message
+    :return: dict of attributes
+    """
+    msg_attributes = {
+        "message": msg.message,
+        "type": "text",
+        "duration": "",
+        "to_id": "",
+    }
+
+    if hasattr(msg.to_id, "user_id"):
+        msg_attributes["to_id"] = msg.to_id.user_id
+    else:
+        msg_attributes["to_id"] = msg.to_id
+
+    if msg.sticker:
+        for attribute in msg.sticker.attributes:
+            if isinstance(attribute, telethon.tl.types.DocumentAttributeSticker):
+                msg_attributes["message"] = attribute.alt
+                msg_attributes["type"] = "sticker"
+
+    elif msg.video:
+        for attribute in msg.video.attributes:
+            if isinstance(attribute, telethon.tl.types.DocumentAttributeVideo):
+                msg_attributes["duration"] = attribute.duration
+                msg_attributes["type"] = "video"
+
+    elif msg.voice:
+        for attribute in msg.voice.attributes:
+            if isinstance(attribute, telethon.tl.types.DocumentAttributeAudio):
+                msg_attributes["duration"] = attribute.duration
+                msg_attributes["type"] = "voice"
+
+    elif msg.photo:
+        msg_attributes["type"] = "photo"
+
+    return msg_attributes
+
+
 async def download_dialog(client, id, MSG_LIMIT):
     """
     Download messages and their metadata for a specific message id,
@@ -78,8 +122,6 @@ async def download_dialog(client, id, MSG_LIMIT):
     """
     try:
         tg_entity = await client.get_entity(id)
-        # path = await client.download_media(id)
-        # print(path)
         messages = await client.get_messages(tg_entity, limit=MSG_LIMIT)
     except ValueError:
         errmsg = f"No such ID found: #{id}"
@@ -88,22 +130,19 @@ async def download_dialog(client, id, MSG_LIMIT):
     dialog = []
 
     for m in messages:
-        if m.media:
-            #path = await m.download_media(file="data/media/")
-            print(type(m.media))
-        if hasattr(m.to_id, "user_id"):
-            to_id = m.to_id.user_id
-        else:
-            to_id = m.to_id
+
+        msg_attrs = msg_handler(m)
 
         dialog.append(
             {
                 "id": m.id,
                 "date": m.date,
                 "from_id": m.from_id,
-                "to_id": to_id,
+                "to_id": msg_attrs["to_id"],
                 "fwd_from": m.fwd_from,
-                "message": m.message,
+                "message": msg_attrs["message"],
+                "type": msg_attrs["type"],
+                "duration": msg_attrs["duration"],
             }
         )
 
